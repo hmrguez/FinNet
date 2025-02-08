f3a9cf6c91498b6393bbc5163ea12d405142fd2b
hmrguez <zealot.algo@gmail.com>
2025-02-08 08:42:38 -0500

    Missing advanced

--- src/langchain/subgraph.py
+++ src/langchain/subgraph.py
@@ -59,3 +59,38 @@ class SubgraphCreator:
             "nodes": [self.graph.nodes[node] for node in subgraph.nodes],
             "edges": [self.graph.edges[edge] for edge in subgraph.edges]
         }
+    
+    def get_advanced_connected_subgraph(self, nodes: List[Tuple[str, float]], edges: List[Tuple[int, float]]) -> Dict[str, List[Any]]:
+        node_ids = {node[0] for node in nodes}
+        edge_ids = {edge[0] for edge in edges}
+        
+        subgraph_nodes = set(node_ids)
+        subgraph_edges = set()
+        
+        for u, v, data in self.graph.edges(data=True):
+            if data.get('id') in edge_ids:
+                subgraph_edges.add((u, v))
+                subgraph_nodes.add(u)
+                subgraph_nodes.add(v)
+        
+        # Ensure connectivity by finding the minimum spanning tree over required nodes
+        induced_subgraph = self.graph.subgraph(subgraph_nodes).copy()
+        
+        if not nx.is_connected(induced_subgraph):
+            # Find shortest paths between all required nodes to enforce connectivity
+            extra_edges = set()
+            for n1, n2 in combinations(subgraph_nodes, 2):
+                if nx.has_path(self.graph, n1, n2):
+                    path = nx.shortest_path(self.graph, n1, n2)
+                    for i in range(len(path) - 1):
+                        extra_edges.add((path[i], path[i + 1]))
+            
+            subgraph_edges.update(extra_edges)
+            subgraph_nodes.update({node for edge in extra_edges for node in edge})
+        
+        final_subgraph = self.graph.subgraph(subgraph_nodes).copy()
+        
+        return {
+            "nodes": [self.graph.nodes[node] for node in final_subgraph.nodes],
+            "edges": [self.graph.edges[edge] for edge in final_subgraph.edges]
+        }
